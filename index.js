57d3a238bc1935cb00d0ea7386ae43f7ff9d5c23
Zac Smith <zrs3141592@gmail.com>
2020-03-03 15:30:00 -0700

    GET working with simple array

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require("express");
 
 const server = express();
+server.use(express.json());
 
-const PORT = 5000;
+let books = [
+  {
+    title: "Happy days",
+    pub: 1975
+  }
+];
+server.get("/", (req, res) => {
+  res.status(200).json({
+    message: `GET is running`
+  });
+});
+
+server.get("/api/books", (req, res) => {
+  res.status(200).json(books);
+});
+
+const PORT = 5150;
 server.listen(PORT, () =>
-  console.log(`\n Server is listening on port ${PORT} **\n`)
+  console.log(`\n** Server is listening on port ${PORT} **\n`)
 );
